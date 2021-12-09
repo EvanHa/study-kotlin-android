9cbe1eb7d00071dce92ee5920f7d48f80cd9afd6
Evan <bc.0x4861@gmail.com>
2021-12-09 16:03:39 +0900

    when

--- kotlinlang/src/main/kotlin/main.kt
+++ kotlinlang/src/main/kotlin/main.kt
@@ -1,25 +1,29 @@
 fun main(args: Array<String>) {
-    println("max : " + maxJavaStyle(1,2))
-    println("max : " + maxKotlinStyle(1,2))
-}
+    // kotlin에서는 switch가 없고, when이 있음
 
-fun maxJavaStyle(a:Int, b:Int):Int {
-    var max:Int
-    if (a > b) {
-        max = a
-    } else {
-        max = b
+    var x = 1
+    when (x) {
+        1 -> println("x == 1")
+        2 -> println("x == 2")
+        else -> {
+            println("x is neither 1 nor 2")
+        }
     }
-    return max
-}
 
-fun maxKotlinStyle(a:Int, b:Int):Int {
-    val max = if (a>b) {
-        println("Choose a")
-        a
-    } else {
-        println("Choose b")
-        b
+    x = 0
+    when (x) {
+        0, 1 -> println("x == 0 or x == 1")
+        else -> println("otherwise")
     }
-    return max
+
+    when (x) {
+        in 1..10 -> println("x는 1부터 10 범위 안에 있음")
+        !in 10..20 -> println("x는 10부터 20 범위 안에 없음")
+        else -> println("otherwise")
+    }
+
+    when (x) {
+        is Int -> println("x는 인트형")
+        else -> println("x는 인트형이 아님")
+    }
 }
