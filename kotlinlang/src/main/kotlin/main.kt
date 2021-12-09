232c8376f4d3862b95b7f7f58495ed653b1d6330
Evan <bc.0x4861@gmail.com>
2021-12-09 12:10:32 +0900

    variable and type

--- kotlinlang/src/main/kotlin/main.kt
+++ kotlinlang/src/main/kotlin/main.kt
@@ -1,15 +1,43 @@
 fun main(args: Array<String>) {
+    // 한번만 할당 되는 val
+    val a: Int = 1
+    val b = 2
+    val c = 3.14
+    val d: String
+    d = "필수로 있어야 하는 구문"
+    // d = "d의 초기값이 없으면 null이 될 수 있는데, d는 null이 될 수 없기 때문
+    val e: String?
+
+    // 여러번 값 할당이 되는 var
+    var f: String = "첫번째 초기화"
+    f = "두번째 초기화"
     println("Hello World!")
-    println("1 + 2 = " + sum(1, 2))
-    println("1 + 2 = " + sum2(1, 2))
-    println("1, 2 max ? " + max(1, 2))
+    println("a = "+a)
+    println("b = "+b)
+    println("c = "+c)
+    println("d = "+d)
+    println("f = "+f)
+
+    // Byte
+    // Short
+    // Int : 123
+    // Long : 123L
+    // Float : 123.4f
+    // Double : 123.4
+    // Char
+    // String
+    var str:String = "abcd"
+    str = "abcd" + 1
+    println("str : " + str)
+    str = "abcd" + "efg"
+    println("str : " + str)
+
+    // Boolean
+    val myTrue: Boolean = true
+    val myFalse: Boolean = false
+    // ?가 붙으면 true, false 외에 null 값도 가질수 있음. nullable
+    val boolNull: Boolean? = null
+    println("true " + myTrue)
+    println("false " + myFalse)
+    println("boolNull " + boolNull)
 }
-
-fun sum(a: Int, b: Int): Int {
-    return a + b
-}
-
-fun sum2(a: Int, b: Int) = a + b
-
-fun max(a: Int, b: Int) = if (a > b) a else b
-
