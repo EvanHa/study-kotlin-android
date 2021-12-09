05641472ab8747b2141b362dad09e72b823dbc53
Evan <bc.0x4861@gmail.com>
2021-12-09 12:17:21 +0900

    for loop

--- kotlinlang/src/main/kotlin/main.kt
+++ kotlinlang/src/main/kotlin/main.kt
@@ -1,43 +1,25 @@
 fun main(args: Array<String>) {
-    // 한번만 할당 되는 val
-    val a: Int = 1
-    val b = 2
-    val c = 3.14
-    val d: String
-    d = "필수로 있어야 하는 구문"
-    // d = "d의 초기값이 없으면 null이 될 수 있는데, d는 null이 될 수 없기 때문
-    val e: String?
+    for (i in 1..5) {
+        print("" + i + " ")
+        if (i==5) println("")
+    }
+    // 1 2 3 4 5
 
-    // 여러번 값 할당이 되는 var
-    var f: String = "첫번째 초기화"
-    f = "두번째 초기화"
-    println("Hello World!")
-    println("a = "+a)
-    println("b = "+b)
-    println("c = "+c)
-    println("d = "+d)
-    println("f = "+f)
+    for (i in 6 downTo 0 step 2) {
+        print("" + i + " ")
+        if (i==0) println("")
+    }
+    // 6 4 2 0
 
-    // Byte
-    // Short
-    // Int : 123
-    // Long : 123L
-    // Float : 123.4f
-    // Double : 123.4
-    // Char
-    // String
-    var str:String = "abcd"
-    str = "abcd" + 1
-    println("str : " + str)
-    str = "abcd" + "efg"
-    println("str : " + str)
+    for (i in 1..5 step 3) {
+        print("" + i + " ")
+    }
+    println("")
+    // 1 4
 
-    // Boolean
-    val myTrue: Boolean = true
-    val myFalse: Boolean = false
-    // ?가 붙으면 true, false 외에 null 값도 가질수 있음. nullable
-    val boolNull: Boolean? = null
-    println("true " + myTrue)
-    println("false " + myFalse)
-    println("boolNull " + boolNull)
+    val numberList = listOf(100, 200, 300)
+    for (number in numberList) {
+        println(number)
+    }
+    // 100 200 300
 }
