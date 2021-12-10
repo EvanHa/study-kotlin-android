e0d6a59ef7916e52ac9ea0916d8ee69de6478695
Evan <bc.0x4861@gmail.com>
2021-12-10 14:22:34 +0900

    lazy init

--- kotlinlang/src/main/kotlin/main.kt
+++ kotlinlang/src/main/kotlin/main.kt
@@ -1,24 +1,20 @@
-// lateinit
-// NullSafe한 코드를 작성하기 위해서 non-null type으로 변수를 선언하는데,
-// 초기값이 없는 경우에는, 이 type을 사용할 수 없는 문제가 있음
-// 이럴 때 lateinit을 사용
-// val, primitive type에서는 사용 불가능
-// lateinit val area: Area // compile error
-// lateinit var width: Int // compile error
-// 초기화를 하지 않고 사용할 수는 없음. 선언은 해두고 나중에 초기화해서 사용할때 사용
+// Lazy initialization
+// 선언할때, 초기화도 같이 넣는다.
+// 다만 실제로 처음 사용될때 초기화 구문이 실행되면서 초기값이 할당됨.
+// val property만 사용할 수 있음
+// primitive type(Int, Boolean등)도 사용 가능
+// Non-null, Nullable 모두 사용 가능
 
-class Rectangle {
-    //var area: Area // Error
-    lateinit var area: Area
-    fun initArea(param: Area): Unit {
-        this.area = param
+class Account() {
+    val balance: Int by lazy {
+        // 처음 사용할때 한번만 호출되고, 그뒤로는 호출되지 않는다.
+        println("Setting balance!")
+        100
     }
 }
 
-class Area(val value: Int)
-
 fun main(args: Array<String>) {
-    val rectangle = Rectangle()
-    rectangle.initArea(Area(10))
-    println(rectangle.area.value)
+    val account = Account()
+    println(account.balance)
+    println(account.balance)
 }
