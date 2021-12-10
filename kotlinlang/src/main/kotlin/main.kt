bab98e8eb59cc770636ac637028923edcf6f2d35
Evan <bc.0x4861@gmail.com>
2021-12-10 13:59:43 +0900

    lateinit

--- kotlinlang/src/main/kotlin/main.kt
+++ kotlinlang/src/main/kotlin/main.kt
@@ -1,20 +1,20 @@
-// Lambda expression
+// lateinit
+// NullSafe한 코드를 작성하기 위해서 non-null type으로 변수를 선언하는데,
+// 초기값이 없는 경우에는, 이 type을 사용할 수 없는 문제가 있음
+// 이럴 때 lateinit을 사용
 
-// JAVA
-/*
-button.setOnClickListener(new View.OnClickListener()) {
-    @Override
-    public void onClick(View view) {
-        ...
+class Rectangle {
+    //var area: Area // Error
+    lateinit var area: Area
+    fun initArea(param: Area): Unit {
+        this.area = param
     }
 }
-*/
 
-// Kotlin
-// button.setOnClickListener { v-> }
+class Area(val value: Int)
 
 fun main(args: Array<String>) {
-    val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
-
-    println(sum(1,2))
+    val rectangle = Rectangle()
+    rectangle.initArea(Area(10))
+    println(rectangle.area.value)
 }
