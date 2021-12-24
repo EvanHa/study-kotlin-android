1034b0a2636aa1cc47f08a231a2a23b275fe29e9
Evan <bc.0x4861@gmail.com>
2021-12-24 21:53:26 +0900

    timer-3
    -SoundPool
    -ActionBar 제거

--- timer/app/src/main/java/com/example/timer/MainActivity.kt
+++ timer/app/src/main/java/com/example/timer/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.timer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,25 +25,36 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private val soundPool = SoundPool.Builder().build()
+
     private var currentCountDownTimer: CountDownTimer? = null
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+        initSounds()
+
+        val actionbar: ActionBar? = supportActionBar;
+        actionbar?.hide()
     }
 
     override fun onResume() {
         super.onResume()
+        soundPool.autoResume()
     }
 
     override fun onPause() {
         super.onPause()
+        soundPool.autoPause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        soundPool.release()
     }
 
     private fun bindViews() {
@@ -75,6 +88,11 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+
     private fun createCountDownTimer(initialMillis: Long) =
         object: CountDownTimer(initialMillis, 1000L) {
             override fun onTick(millisUntilFinished: Long) {
@@ -83,7 +101,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-
+                completeCountDown()
             }
         }
 
@@ -91,11 +109,25 @@ class MainActivity : AppCompatActivity() {
         currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
         currentCountDownTimer?.start()
 
+        tickingSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+        }
     }
 
     private fun stopCountDown() {
         currentCountDownTimer?.cancel()
         currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun completeCountDown() {
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let { soundId ->
+            soundPool.play(soundId, 1F, 1F, 0, 0, 1F)
+        }
     }
 
     private fun updateRemainTime(remainMillis: Long) {
