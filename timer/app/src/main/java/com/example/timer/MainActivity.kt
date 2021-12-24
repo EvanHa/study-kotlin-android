45a841dc39d18d9a02702c59951d1c16e95bc649
Evan <bc.0x4861@gmail.com>
2021-12-24 21:23:01 +0900

    timer-2
    -CountDownTimer

--- timer/app/src/main/java/com/example/timer/MainActivity.kt
+++ timer/app/src/main/java/com/example/timer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
@@ -22,11 +23,25 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.seekBar)
     }
 
+    private var currentCountDownTimer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bindViews()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
     private fun bindViews() {
@@ -37,18 +52,60 @@ class MainActivity : AppCompatActivity() {
                     progress: Int,
                     fromUser: Boolean
                 ) {
-                    Log.d(TAG, "SeekBar Changed")
-                    remainMinutesTextView.text = "%02d".format(progress)
+                    if (fromUser) {
+                        Log.d(TAG, "SeekBar Changed")
+                        updateRemainTime(progress * 60 * 1000L)
+                    }
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
+                    stopCountDown()
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?: return
 
+                    if (seekBar.progress == 0) {
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
                 }
             }
         )
+    }
+
+    private fun createCountDownTimer(initialMillis: Long) =
+        object: CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+
+    private fun startCountDown() {
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+        currentCountDownTimer?.start()
+
+    }
+
+    private fun stopCountDown() {
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+    }
+
+    private fun updateRemainTime(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsdTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
     }
 }
