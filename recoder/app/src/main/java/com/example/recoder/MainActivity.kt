302b71bf871e15df8dbac22a00686b8060a586c6
H.memerlism <bc.0x4861@gmail.com>
2022-01-15 19:49:08 +0900

    recorder-3
    녹음된 파일 재생 부분 수정

--- recoder/app/src/main/java/com/example/recoder/MainActivity.kt
+++ recoder/app/src/main/java/com/example/recoder/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.recoder
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MainActivity"
@@ -20,7 +25,8 @@ class MainActivity : AppCompatActivity() {
 
     private val requiredPermissions = arrayOf(
             Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     private val recordingFilePath: String by lazy {
         "${externalCacheDir?.absolutePath}/recording.3gp"
@@ -60,7 +66,11 @@ class MainActivity : AppCompatActivity() {
 
         // 권한이 주워지지 않은경우
         if (!audioRecordPermissionGranted) {
-            finish()
+            if (!shouldShowRequestPermissionRationale(permissions.first())) {
+                showPermissionExplanationDialog()
+            } else {
+                finish()
+            }
         }
     }
 
@@ -110,6 +120,7 @@ class MainActivity : AppCompatActivity() {
                 }
         recorder?.start()
         state = State.ON_RECORDING
+        Log.d(TAG, "recording Path : " + recordingFilePath)
     }
 
     private fun stopRecording() {
@@ -128,6 +139,11 @@ class MainActivity : AppCompatActivity() {
                     prepare()
                     // 스트리밍의 경우, prepareAsync를 사용
                 }
+        player?.setOnCompletionListener {
+            stopPlaying()
+            state = State.AFTER_RECORDING
+        }
+        player?.start()
         state = State.ON_PLAYING
     }
 
@@ -135,6 +151,21 @@ class MainActivity : AppCompatActivity() {
         player?.release()
         player = null
         state = State.AFTER_RECORDING
+    }
+
+    private fun showPermissionExplanationDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("녹음 권한을 켜주셔야지 앱을 정상적으로 사용할 수 있습니다. 앱 설정 화면으로 진입하셔서 권한을 켜주세요.")
+            .setPositiveButton("권한을 변경하러 가기") {_, _ -> navigateToAppSetting()}
+            .setNegativeButton("앱 종료하기") {_, _ -> finish()}
+            .show()
+    }
+
+    private fun navigateToAppSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
     }
 
     companion object {
