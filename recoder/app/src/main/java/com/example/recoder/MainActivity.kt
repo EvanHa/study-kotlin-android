09261bfc81c4593a5b4ce0a4d3cc4354c51e5089
H.memerlism <bc.0x4861@gmail.com>
2022-01-15 17:10:12 +0900

    recoder-2
    state 관리
    audio recording
    audio play

--- recoder/app/src/main/java/com/example/recoder/MainActivity.kt
+++ recoder/app/src/main/java/com/example/recoder/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.recoder
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MainActivity"
@@ -11,9 +14,28 @@ class MainActivity : AppCompatActivity() {
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.recordButton)
     }
+    private val resetButton: Button by lazy {
+        findViewById(R.id.resetButton)
+    }
 
-    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private val requiredPermissions = arrayOf(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
     private var state = State.BERFORE_RECORDING
+        set(value) {
+            field = value
+            resetButton.isEnabled =
+                    (value == State.AFTER_RECORDING) ||
+                            (value == State.ON_PLAYING)
+            recordButton.updateIconWithState(value)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +43,8 @@ class MainActivity : AppCompatActivity() {
 
         requestAudioPermission()
         initViews()
+        bindViews()
+        initVariables()
     }
 
     override fun onRequestPermissionsResult(
@@ -46,6 +70,71 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        recordButton.setOnClickListener {
+            when(state) {
+                State.BERFORE_RECORDING -> {
+                    startRecording()
+                }
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+        resetButton.setOnClickListener {
+            stopPlaying()
+            state = State.BERFORE_RECORDING
+        }
+    }
+
+    private fun initVariables() {
+        state = State.BERFORE_RECORDING
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder()
+                .apply {
+                    setAudioSource(MediaRecorder.AudioSource.MIC)
+                    setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                    setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+                    setOutputFile(recordingFilePath)
+                    prepare()
+                }
+        recorder?.start()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer()
+                .apply {
+                    setDataSource(recordingFilePath)
+                    prepare()
+                    // 스트리밍의 경우, prepareAsync를 사용
+                }
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
     }
 
     companion object {
